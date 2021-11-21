a806dfa29563971d58b716d6b879f81883a81b82
Alberto Zanotti <albizan@hotmail.it>
2021-11-21 22:34:26 +0100

    Connect to mongo db

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,45 @@
+import "./env";
+
 import ActorReader from "./ActorReader";
 import MovieReader from "./MovieReader";
+import { resolve } from "path";
+import { InsertManyResult, MongoClient, MongoClientOptions } from "mongodb";
+import Actor from "./Actor";
+import Movie from "./Movie";
 
 async function start() {
-    const movieReader = new MovieReader(__dirname + "/../files/movies.tsv");
+    const client = new MongoClient(process.env.DB_CONN_STRING as string);
+    await client.connect();
+    console.log("Connected to MongoDB");
+    const actorsCollection = client.db("imdb").collection("actors");
+    const moviesCollection = client.db("imdb").collection("movies");
+
+    const movieReader = new MovieReader(resolve(__dirname, "../files/movies.tsv"));
     await movieReader.read();
 
-    const actorReader = new ActorReader(__dirname + "/../files/names.tsv", movieReader.movieMap);
+    const actorReader = new ActorReader(resolve(__dirname, "../files/names.tsv"), movieReader.movieMap);
     await actorReader.read();
 
     console.log("Total movies found:", movieReader.movieMap.size);
     console.log("Total actors found:", actorReader.actorMap.size);
 
-    actorReader.actorMap.forEach((value) => {
-        console.log(value);
+    // Once we have actors and movies, start inserting them on DB
+    let actorsBuffer: Actor[] = [];
+    let actorsBufferSize = 0;
+    let actorsPromises: Promise<InsertManyResult>;
+    let moviesBuffer: Movie[] = [];
+    let moviesBufferSize = 0;
+
+    actorReader.actorMap.forEach((actor: Actor) => {
+        actorsBuffer.push(actor);
+        actorsBufferSize++;
+        if (actorsBufferSize % 1000 === 0) {
+            // Do not await
+            let promise = actorsCollection.insertMany(actorsBuffer);
+            console.log("1000 actors inserted");
+            actorsBuffer = [];
+            actorsBufferSize = 0;
+        }
     });
 }
 

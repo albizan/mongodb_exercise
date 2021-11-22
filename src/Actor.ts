0a41a5c2ffac336e7898fa81483b26dc21f99110
Alberto Zanotti <albizan@hotmail.it>
2021-11-22 12:56:04 +0100

    movies array is not required

--- src/Actor.ts
+++ src/Actor.ts
@@ -1,31 +1,41 @@
 import Movie from "./Movie";
 
 export default class Actor {
-    id: string;
-    name: string;
-    birthYear?: number;
-    deathYear?: number;
-    roles: string[];
-    movies: Movie[];
+  id: string;
+  name: string;
+  birthYear?: number;
+  deathYear?: number;
+  roles: string[];
+  movies?: any;
 
-    constructor(id: string, name: string, birthYear: string, deathYear: string, roles: string) {
-        this.id = id;
-        this.name = name;
-        this.birthYear = this.parseNumber(birthYear);
-        this.deathYear = this.parseNumber(deathYear);
-        this.roles = roles ? roles.split(",") : [];
-        this.movies = [];
-    }
+  constructor(
+    id: string,
+    name: string,
+    birthYear: string,
+    deathYear: string,
+    roles: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.birthYear = this.parseNumber(birthYear);
+    this.deathYear = this.parseNumber(deathYear);
+    this.roles = roles ? roles.split(",") : [];
+    this.movies = [];
+  }
 
-    addMovie(movie: Movie) {
-        this.movies.push(movie);
-    }
+  addMovie(movie: Movie) {
+    const partialMovie = {
+      ...movie,
+    };
+    delete partialMovie.actors;
+    this.movies.push(partialMovie);
+  }
 
-    parseNumber(value: string): number | undefined {
-        const parsed = parseInt(value, 10);
-        if (isNaN(parsed)) {
-            return;
-        }
-        return parsed;
+  parseNumber(value: string): number | undefined {
+    const parsed = parseInt(value, 10);
+    if (isNaN(parsed)) {
+      return;
     }
+    return parsed;
+  }
 }

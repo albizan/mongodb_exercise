8d344f1c047cc82d83b32e3094ae59efbe73e294
Alberto Zanotti <albizan@hotmail.it>
2021-11-22 12:56:16 +0100

    Actor array is not required

--- src/Movie.ts
+++ src/Movie.ts
@@ -1,29 +1,44 @@
 import Actor from "./Actor";
 
 export default class Movie {
-    id: string;
-    title: string;
-    startYear?: number;
-    endYear?: number;
-    length?: number;
-    genres: string[];
-    actors: Actor[];
+  id: string;
+  title: string;
+  startYear?: number;
+  endYear?: number;
+  length?: number;
+  genres: string[];
+  actors?: any;
 
-    constructor(id: string, title: string, startYear: string, endYear: string, length: string, genres: string) {
-        this.id = id;
-        this.title = title;
-        this.startYear = this.parseNumber(startYear);
-        this.endYear = this.parseNumber(endYear);
-        this.length = this.parseNumber(length);
-        this.genres = genres.split(",");
-        this.actors = [];
-    }
+  constructor(
+    id: string,
+    title: string,
+    startYear: string,
+    endYear: string,
+    length: string,
+    genres: string
+  ) {
+    this.id = id;
+    this.title = title;
+    this.startYear = this.parseNumber(startYear);
+    this.endYear = this.parseNumber(endYear);
+    this.length = this.parseNumber(length);
+    this.genres = genres.split(",");
+    this.actors = [];
+  }
 
-    parseNumber(value: string): number | undefined {
-        const parsed = parseInt(value, 10);
-        if (isNaN(parsed)) {
-            return;
-        }
-        return parsed;
+  parseNumber(value: string): number | undefined {
+    const parsed = parseInt(value, 10);
+    if (isNaN(parsed)) {
+      return;
     }
+    return parsed;
+  }
+
+  addActor(actor: Actor) {
+    const partialActor = {
+      ...actor,
+    };
+    delete actor.movies;
+    this.actors.push(partialActor);
+  }
 }
